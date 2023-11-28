d0a56e5987b8e6adccba95485944f81e05b05052
kIdKib <gyimahrokib@gmail.com>
2023-11-28 19:40:21 +0000

    addition and minus

---
+++ src/Calculatrice.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+
+class Calculatrice extends Component {
+    state = { 
+        items: "",
+        resultat: 0
+    } 
+
+    handleChange = (e) =>{
+        this.setState({ input: Number(e.target.value) })
+    }
+
+    addition = () =>{
+        let valeur = this.state.input + this.state.resultat
+        this.setState({resultat: valeur})
+    }
+
+    soustraction =() =>{
+        let valeur = this.state.resultat - this.state.input
+        this.setState({resultat: valeur})
+    }
+
+
+    render() { 
+        return (
+            <div>
+                <form>
+                    <h1>{this.state.resultat}</h1>
+                    <input type="text" name="" id="" onChange={(e) => this.handleChange(e)}/>
+                    <button onClick={(e) => {e.preventDefault(); this.addition(e)}}>+++++++</button>
+                    <button onClick={(e) => {e.preventDefault(); this.soustraction(e)}}>---------</button>
+                    <h1></h1>
+                </form>
+            </div>
+        );
+    }
+}
+ 
+export default Calculatrice;
